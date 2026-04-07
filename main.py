fb0507a434f1440b5619e59c3bf1532d6866120f
Ira <ira@gmail.com>
2026-04-07 23:05:58 +0300

    Добавил функцию print_author() с чтением переменной AUTHOR из .env

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
-print(Hello from repository!)
+print('Hello from repository!')
+import os
+from dotenv import load_dotenv
+
+def print_author():
+    # Загружаем переменные из файла .env
+    load_dotenv()
+    
+    # Читаем значение переменной AUTHOR
+    author = os.getenv("AUTHOR")
+    
+    # Печатаем результат
+    print(f"Автор проекта: {author}")
+
+# Вызов функции (для проверки)
+if __name__ == "__main__":
+    print_author()
